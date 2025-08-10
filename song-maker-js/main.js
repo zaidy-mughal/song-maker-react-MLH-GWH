27d6bc6d1055e1d991cf2109d7c656efa341ad20
utk09-NCL <utkarsh.t0298@gmail.com>
2025-08-10 11:44:08 +0100

    Session 3: create divs for NOTES

--- song-maker-js/main.js
+++ song-maker-js/main.js
@@ -1,20 +1,51 @@
+/**
+ * JSDoc.
+ * Create a HTML element with provided class and id
+ * @param {string} tag HTML tag
+ * @param {string} cssClass class for the HTML tag
+ * @param {string} idAttr id for the HTML tag
+ * @returns {HTMLElement}
+ * @example const myElement = el("span", "dummy-class", "dummy-id");
+ * // It will create
+ * <span class='dummy-class' id='dummy-id'></span>
+ */
+function el(tag, cssClass, idAttr) {
+  const htmlElement = document.createElement(tag);
+  if (cssClass) {
+    htmlElement.className = cssClass;
+  }
+  if (idAttr) {
+    htmlElement.id = idAttr;
+  }
+
+  return htmlElement;
+}
+
+const ROWS = 8; // total number of horizontal pitches
+const COLS = 16; // total number of vertical time steps
+/**
+ * Names for each row / note names.
+ * Numbers 3 and 2 refer to octaves. It is arranged in the order of highest note first.
+ * More info: {@link https://www.musicandtheory.com/an-easy-guide-to-scientific-pitch-notation/}
+ */
+const NOTE_NAMES = ["C3", "B2", "A2", "G2", "F2", "E2", "D2", "C2"];
+
 const grid = document.getElementById("grid");
 
-const gridLayout = document.createElement("div");
-gridLayout.className = "grid-layout";
-gridLayout.id = "grid-layout";
-
-const labelsLeftSide = document.createElement("div");
-labelsLeftSide.className = "labels-left-side";
-
-const rulerSpacer = document.createElement("div");
-rulerSpacer.className = "ruler-spacer";
-
-const rowLabel = document.createElement("div");
-rowLabel.className = "row-label";
-
+const gridLayout = el("div", "grid-layout", "grid-layout-1"); // outer grid layout container
+// Left column: notes labels
+const labelsLeftSide = el("div", "labels-left-side", "labels-left-side-1");
+const rulerSpacer = el("div", "ruler-spacer");
 labelsLeftSide.appendChild(rulerSpacer);
-labelsLeftSide.appendChild(rowLabel);
+
+const rowLabels = el("div", "row-labels"); // container for note labels
+NOTE_NAMES.forEach((noteName) => {
+  const rowLabel = el("div", "row-label");
+  rowLabel.textContent = noteName; // set the label text
+  rowLabels.appendChild(rowLabel);
+});
+
+labelsLeftSide.appendChild(rowLabels);
 
 gridLayout.appendChild(labelsLeftSide);
 
